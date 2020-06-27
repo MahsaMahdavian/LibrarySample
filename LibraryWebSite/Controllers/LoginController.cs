05990dce6af0db52228f1fba02fee19e83a979f4
mahsamahdavian <mahsaa.mahdaviaan@gmail.com>
2020-06-27 09:09:06 +0430

    AddLogin

---
+++ LibraryWebSite/Controllers/LoginController.cs
@@ -0,0 +1,109 @@
+﻿
+using System.Threading.Tasks;
+using LibraryWebSite.Entities.Identity;
+using LibraryWebSite.Services.Contracts;
+using LibraryWebSite.ViewModel.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LibraryWebSite.Controllers
+{
+    
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IApplicationUserManager _userManager;
+        private readonly IApplicationRoleManager _roleManager;
+        private readonly IHttpContextAccessor _accessor;
+        public LoginController
+            (
+            IApplicationUserManager userManager,
+            SignInManager<User> signInManager,
+            ILogger<LoginController> logger,
+            IHttpContextAccessor accessor,
+            IApplicationRoleManager roleManager
+            )
+        {
+            _accessor = accessor;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _logger = logger;
+
+        }
+       
+      
+        public IActionResult Index(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel viewModel, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var User = await _userManager.FindByNameAsync(viewModel.UserName);
+                if (User != null)
+                {
+                    if (User.IsActive)
+                    {
+                        var result = await _signInManager.PasswordSignInAsync(viewModel.UserName, viewModel.Password, viewModel.RememberMe, true);
+                        if (result.Succeeded)
+                        {
+                            _logger.LogInformation(1, $"{viewModel.UserName} logged in.");
+                            if (Url.IsLocalUrl(returnUrl))
+                            {
+                                return Redirect(returnUrl);
+                            }
+                            return RedirectToAction("Index","Book", new { area = "Admin" });
+                        }
+
+                        else if (result.IsLockedOut)
+                            ModelState.AddModelError(string.Empty, "حساب کاربری شما به مدت 20 دقیقه به دلیل تلاش های ناموفق قفل شد.");
+
+                        else if (result.RequiresTwoFactor)
+                            return View(viewModel);
+
+                        else
+                        {
+                            _logger.LogWarning($"The user attempts to login with the IP address({_accessor.HttpContext?.Connection?.RemoteIpAddress.ToString()}) and username ({viewModel.UserName}) and password ({viewModel.Password}).");
+                            ModelState.AddModelError(string.Empty, "نام کاربری یا کلمه عبور شما صحیح نمی باشد.");
+                        }
+                    }
+                    else
+                        ModelState.AddModelError(string.Empty, "حساب کاربری شما غیرفعال است.");
+                }
+
+                else
+                {
+                    _logger.LogWarning($"The user attempts to login with the IP address({_accessor.HttpContext?.Connection?.RemoteIpAddress.ToString()}) and username ({viewModel.UserName}) and password ({viewModel.Password}).");
+                    ModelState.AddModelError(string.Empty, "نام کاربری یا کلمه عبور شما صحیح نمی باشد.");
+                }
+            }
+
+            return View(viewModel); 
+        }
+
+
+        public async Task<IActionResult> SignOut()
+        {
+            var user = User.Identity.IsAuthenticated ? await _userManager.FindByNameAsync(User.Identity.Name) : null;
+            await _signInManager.SignOutAsync();
+            if (user != null)
+            {
+                await _userManager.UpdateSecurityStampAsync(user);
+                _logger.LogInformation(4, $"{user.UserName} logged out.");
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+    }
+}
